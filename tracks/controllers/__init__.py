0931c3018c350d69785bbff03028a3f430bed34b
Jeongmin Byun <jmbyun91@gmail.com>
2026-02-22 20:48:56 +0900

    fix router

--- tracks/controllers/__init__.py
+++ tracks/controllers/__init__.py
@@ -5,7 +5,7 @@ API route controllers.
 from .chat import router as chat_router
 from .history import router as history_router
 from .heartbeat import router as heartbeat_router
-from .agents import router as agents_router
+# from .agents import router as agents_router
 from .settings import router as settings_router
 from .connection.google import router as connection_google_router
 from .connection.instagram import router as connection_instagram_router
@@ -27,5 +27,5 @@ routers = [
     connection_twitter_router,
     connection_smartthings_router,
     connection_youtube_router,
-    agents_router,
+    # agents_router,
 ]
